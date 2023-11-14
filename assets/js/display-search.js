3745abe9222d04ad1b5632ac720785c153cadb27
Mike Sowers <msowers72@yahoo.com>
2023-11-13 20:15:08 -0500

    add getParams function

--- assets/js/display-search.js
+++ assets/js/display-search.js
@@ -2,7 +2,17 @@ var resultTextEl = document.querySelector('#result-text');
 var resultContentEl = document.querySelector('#result-content');
 var searchFormEl = document.querySelector('#search-form')
 
+function getParams() {
+    // Get the search params out of the URL (i.e. `?q=london&format=photo`) and convert it to an array (i.e. ['?q=london', 'format=photo'])
+    var searchParamsArr = document.location.search.split('&');
+   
 
+    // get the query and format values
+    var query = searchParamsArr[0].split('=').pop();
+    var format = searchParamsArr[1].split('=').pop();
+    
+    // searchApi(query, format)
+}
 
 
 function handleSearchFormSubmit(event) {
@@ -13,6 +23,7 @@ function handleSearchFormSubmit(event) {
 
     if(!searchInputVal) {
         console.error('You need a search input value!')
+        return;
     }
     
     // searchApi(searchInputVal, formatInputVal)
@@ -21,3 +32,5 @@ function handleSearchFormSubmit(event) {
 
 
 searchFormEl.addEventListener('submit', handleSearchFormSubmit);
+
+getParams()
