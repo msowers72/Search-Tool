f715fdde126c13a3994fb516356cbbfa06d9820b
Mike Sowers <msowers72@yahoo.com>
2023-11-14 20:24:38 -0500

    added searchAPI function to dispalysearchjs

--- assets/js/display-search.js
+++ assets/js/display-search.js
@@ -4,14 +4,51 @@ var searchFormEl = document.querySelector('#search-form')
 
 function getParams() {
     // Get the search params out of the URL (i.e. `?q=london&format=photo`) and convert it to an array (i.e. ['?q=london', 'format=photo'])
-    var searchParamsArr = document.location.search.split('&');
-   
+    var searchParamsArr = document.location.search.split('&');   
 
     // get the query and format values
     var query = searchParamsArr[0].split('=').pop();
     var format = searchParamsArr[1].split('=').pop();
     
     // searchApi(query, format)
+}
+
+function searchApi(query, format) {
+    var locQueryUrl = 'https://www.loc.gov/search/?fo=json';
+
+    if(format) {
+        locQueryUrl = 'https://www.loc.gov/' + format + '/?fo=json';
+    }
+
+    locQueryUrl = locQueryUrl + '&q=' + query;
+
+    fetch(locQueryUrl)
+    .then(function (response) {
+        if(!response.ok) {
+            throw response.json();
+        }
+
+        return response.json();
+    })
+    .then(function(locRes) {
+        // write query to page so user knows what they are viewing
+        resultTextEl.textContent = locRes.search.query;
+
+        console.log(locRes)
+
+        if(!locRes.results.length) {
+            console.llog('No results found!');
+            resultContentEl.innerHTML = '<h3>No results found, search again!<h3>'
+        } else {
+            resultContentEl.textContent = '';
+            for(var i = 0; i < locRes.results.length; i++) {
+                printResults(locRes.results[i])
+            }
+        }
+    })
+    .catch(function(error) {
+        console.error(error)
+    })
 }
 
 
